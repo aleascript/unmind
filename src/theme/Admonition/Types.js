6b6e48a947f5ff00f081ea45335a4a8bb9ebc5bd
AleaScript <aleascript@proton.me>
2026-09-03 21:27:56 +0200

    Render design admonitions without an extra wrapper

--- src/theme/Admonition/Types.js
+++ src/theme/Admonition/Types.js
@@ -3,12 +3,11 @@ import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
 
 function DesignAdmonition(props) {
   const Note = DefaultAdmonitionTypes.note;
+  const className = ['theme-admonition-design', props.className]
+    .filter(Boolean)
+    .join(' ');
 
-  return (
-    <div className="theme-admonition-design">
-      <Note {...props} type="note" />
-    </div>
-  );
+  return <Note {...props} type="note" className={className} />;
 }
 
 export default {
