870d8d8d29ab73da09ce8304b0bd40dedfb74bdf
aleascript <aleascript@gmail.com>
2026-09-17 15:00:16 +0200

    publications on sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -3,17 +3,17 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
   docsSidebar: [
     'index',
-    {
-      type: 'link',
-      label: 'Publications',
-      href: '/publications/',
-    },
     'rules',
     {
       type: 'category',
       label: 'Booklets',
       link: {type: 'doc', id: 'booklets'},
       items: ['watch', 'serve', 'please', 'fade', 'order', 'resist'],
+    },
+    {
+      type: 'link',
+      label: 'Publications',
+      href: '/publications/',
     },
   ],
 };
